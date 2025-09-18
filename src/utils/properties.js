3a0ece0a7eab118baf9356b06571afa9a379a00e
edeldev <edeldev25@gmail.com>
2025-09-18 12:14:36 -0600

    feat: customize

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -5,9 +5,8 @@ export const properties = [
     scale: 1,
     price: 20,
     ingredients: [
-      { id: 1, text: "Tocino", price: 2 },
-      { id: 2, text: "Salchicha polaca", price: 4 },
-      { id: 3, text: "Aros de cebolla", price: 1 },
+      { id: 6, text: "Papas fritas" },
+      { id: 7, text: "Doble carne" },
     ],
   },
   {
@@ -16,8 +15,8 @@ export const properties = [
     scale: 1.2,
     price: 30,
     ingredients: [
-      { id: 4, text: "Queso extra", price: 3 },
-      { id: 5, text: "Guacamole", price: 5 },
+      { id: 6, text: "Papas fritas" },
+      { id: 7, text: "Doble carne" },
     ],
   },
   {
@@ -26,8 +25,8 @@ export const properties = [
     scale: 1.4,
     price: 40,
     ingredients: [
-      { id: 6, text: "Papas fritas", price: 4 },
-      { id: 7, text: "Doble carne", price: 7 },
+      { id: 6, text: "Papas fritas" },
+      { id: 7, text: "Doble carne" },
     ],
   },
 ];
